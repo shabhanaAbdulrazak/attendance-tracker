983d1edf01bcea748fc27669606f28d207bc9198
shabhanaAbdulrazak <117266686+shabhanaAbdulrazak@users.noreply.github.com>
2023-06-21 10:29:37 -0400

    Add files via upload

---
+++ message.py
@@ -0,0 +1,6 @@
+import pywhatkit
+ap=59
+if ap<75 and ap>61:
+  pywhatkit.sendwhatmsg("+918072091395","your percentage is below 75%",23,27)
+elif ap<60:
+  pywhatkit.sendwhatmsg_instantly('+917305104325',"Hai mam/sir.Your son/Daughter Attendance percentage is getting low.If it continuous they wont permit to write their semester exam.Thank you")
